91f9e417deb90c646d512afdb54565b2bf9f5aec
Dimitrios Papageorgiou <dmpapageo@gmail.com>
2018-06-15 13:43:02 -0400

    First commit of Crystal Ball

---
+++ Crystal Ball/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Coding Challenge 2
+//
+//  Created by Dimitrios Papageorgiou on 6/8/18.
+//  Copyright © 2018 Dimitrios Papageorgiou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var randomMessageIndex = 0
+    
+    let randomCrystalMessage = [
+        "It's possible",
+        "Certainly",
+        "Ask again later",
+        "If you try hard enough",
+        "I don't think so~",
+        "I have no clue"
+    ]
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newCrystalMessage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var CrystalMessage: UILabel!
+   
+    @IBAction func AskButtonPressed(_ sender: UIButton) {
+    
+        newCrystalMessage()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newCrystalMessage()
+    }
+    
+    func newCrystalMessage(){
+        
+        randomMessageIndex = Int(arc4random_uniform(6)) 
+        
+        CrystalMessage.text = String(randomCrystalMessage[randomMessageIndex])
+
+    }
+}
+
